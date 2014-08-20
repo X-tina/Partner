90b197d1a8df099cd6048b9fbd91ab171115a80a
Kristine <kristinka89@gmail.com>
2014-08-20 15:21:03 +0300

    Refactoring save_project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,24 +10,25 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.create(project_params)
-    @projects = Project.all
+    @project = Project.new(project_params)
+    
+    save_project
   end
 
   def edit
-  	@project = Project.find(params[:id])
+  	@project = project_find
   	render :show_form
   end
 
   def update
-    @project = Project.find(params[:id])
-    @project.update_attributes(project_params)
-    @projects = Project.all
-    render :hide_form
+    @project = project_find
+    @project.assign_attributes(project_params)
+
+    save_project    
   end
 
   def destroy
-    @project = Project.find(params[:id]).destroy
+    @project = project_find.destroy
     @projects = Project.all
   end
 
@@ -35,5 +36,18 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:name)
+  end
+
+  def project_find
+  	Project.find(params[:id])
+  end
+
+  def save_project
+  	if @project.save
+  		@projects = Project.all
+      render :hide_form
+  	else
+      render :show_form
+  	end
   end
 end
