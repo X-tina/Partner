dff78a7120dd0f10c76ff50335f2b90c83c6463c
Kristine <kristinka89@gmail.com>
2014-08-20 10:19:42 +0300

    Adding new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,19 @@ class ProjectsController < ApplicationController
 	def home
     @projects = Project.all
 	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.create(project_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:name)
+	end
 end
