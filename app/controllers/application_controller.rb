31d29f959412613b59ce53b84baa466b61a00f79
Kristine <kristinka89@gmail.com>
2014-08-21 15:30:22 +0300

    Add phone_number to permitted devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def onfigure_permitted_params
+    devise_parameter_sanitize.for(:sign_up) { |s| s.permit(:email, :phone_number, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |a| a.permit(:email, :phone_number, :password, :password_confirmation, :current_password) }
+  end
 end
